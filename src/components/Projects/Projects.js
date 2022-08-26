80a71dff6fe44a1e5b4d4ee5ce0d638de5d1f6b7
ismail saoudi <ismail.saoudi@icloud.com>
2022-08-26 12:54:38 -0400

    styling the project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import homepage from "../../Assets/Projects/homepage.png";
+import noteTaker from "../../Assets/Projects/noteTaker.png";
+import  WritingBlog from "../../Assets/Projects/WritingBlog.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -23,38 +19,38 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={homepage}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Quiz Game"
+              description="Quiz Game using Trivia API to sort questions Randomly."
+              ghLink="https://github.com/ismailsaoudi/Quiz_game"
+              demoLink="https://ismailsaoudi.github.io/Quiz_game/home.html"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={noteTaker}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Note Taker"
+              description="On the fly note Taker helps you take notes and modify them."
+              ghLink="https://github.com/ismailsaoudi/NoteTaker/Bits-0f-C0de"
+              demoLink="https://notetakerisma.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={WritingBlog}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Writing Blog"
+              description="A platform to publish your writings, and the community will have the ability to edit them appropriately to ensure that they’re prepared for publication."
+              ghLink="https://github.com/ismailsaoudi/writers_block"
+              demoLink="https://writers-block-jh.herokuapp.com/login/"              
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
@@ -63,7 +59,7 @@ function Projects() {
               ghLink="https://github.com/soumyajit4419/Plant_AI"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </Container>
